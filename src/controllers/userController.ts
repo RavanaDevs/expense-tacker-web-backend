e947a776e7ab693b71a9f4d07c376919e6e4b2a0
Kavindu Sanjula <123143684+RavanaDevs@users.noreply.github.com>
2024-11-18 13:21:52 +0530

    Update userController.ts

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -75,12 +75,10 @@ export const deleteUser = async (
   try {
     const user = await User.findByIdAndDelete(req.params.id)
     if (!user) {
-      res.status(404).json({ message: 'User not found' })
-    } else {
-      res.status(200).json({ message: 'User deleted successfully' })
+      return res.status(404).json({ message: 'User not found' })
     }
+    res.status(200).json({ message: 'User deleted successfully' })
   } catch (err) {
-    console.log(err)
     next(err)
   }
 }
